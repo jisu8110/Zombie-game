d4df8be5c7591df22375c80d993273d6b55bba45
jisu8110 <jisu811023@gmail.com>
2021-12-19 16:48:12 +0900

    feed kitty

--- Zombie.sol
+++ Zombie.sol
@@ -2,13 +2,11 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
-    // event
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
-    // structure
     struct Zombie {
         string name;
         uint dna;
@@ -16,26 +14,25 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    // mapping
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    // zombie 등록
-    function _createZombie(string _name, uint _dna) private {
+    function _createZombie(string _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
     }
-    // 랜덤 zombie dna 생성
+
     function _generateRandomDna(string _str) private view returns (uint) {
         uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
     }
-    // 랜덤 zombie 생성
+
     function createRandomZombie(string _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
